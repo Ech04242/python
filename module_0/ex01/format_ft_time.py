a70fa6c8f15c87de3cc227215b9bbc384a9714c0
Theo LASSARAT <tlassara@bess-f2r1s19.clusters.42paris.fr>
2024-01-27 12:38:44 +0100

    push ex00 ex01

---
+++ module_0/ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+
+from time import time, localtime, strftime
+
+if __name__== "__main__":
+    sec = time();
+    print("Seconds since January 1, 1970: {:,.4f} or {:.2e} in scientific notation".format(sec, sec))
+    sec = localtime()
+    print (strftime("%b %d %Y", sec))
