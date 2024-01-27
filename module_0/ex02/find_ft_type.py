002a9477fd7908cdcbde883b51b65bb44213c73e
Theo LASSARAT <tlassara@bess-f2r1s19.clusters.42paris.fr>
2024-01-27 12:58:37 +0100

    push ex02

---
+++ module_0/ex02/find_ft_type.py
@@ -0,0 +1,30 @@
+def all_thing_is_obj(obj):
+    if isinstance(obj, list):
+        print(f"List : {type(obj)}")
+    elif isinstance(obj, tuple):
+        print(f"Tuple : {type(obj)}")
+    elif isinstance(obj, set):
+        print(f"Set : {type(obj)}")
+    elif isinstance(obj, dict):
+        print(f"Dict : {type(obj)}")
+    elif obj == "Brian":
+        print(f"{obj} is in the kitchen : {type(obj)}")
+    elif obj == "Toto":
+        print(f"{obj} is in the kitchen : {type(obj)}")
+    else:
+        print("Type not found")
+    return 42
+
+if  __name__== "__main__":
+    ft_list = ["Hello", "tata!"]
+    ft_tuple = ("Hello", "toto!")
+    ft_set = {"Hello", "tutu!"}
+    ft_dict = {"Hello" : "titi!"}
+
+    all_thing_is_obj(ft_list)
+    all_thing_is_obj(ft_tuple)
+    all_thing_is_obj(ft_set)
+    all_thing_is_obj(ft_dict)
+    all_thing_is_obj("Brian")
+    all_thing_is_obj("Toto")
+    print(all_thing_is_obj(10))
