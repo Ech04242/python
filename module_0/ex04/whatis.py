cc044aa0d32aebe04f74bd5bf90235e038833d7b
Theo LASSARAT <tlassara@bess-f2r1s19.clusters.42paris.fr>
2024-01-27 17:05:52 +0100

    push ex04

---
+++ module_0/ex04/whatis.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    if (argc <= 1):
+        print()
+        sys.exit(0)
+    elif (argc >= 3):
+        print("AssertionError: more than one argument is provided")
+    elif (argc == 2):
+        try:
+            nb = int (sys.argv[1])
+            if (nb % 2 == 0):
+                print("I'm Even")
+            else:
+                print("I'm Odd")
+        except:
+            print("AssertionError: argument is not an integer")
