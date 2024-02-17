2301dd0d0f0034faf9a6efac37d952f4c6b3c2d1
Theo LASSARAT <tlassara@bess-f2r1s19.clusters.42paris.fr>
2024-02-17 15:26:50 +0100

    add comentary

--- module_1/ex00/give_bmi.py
+++ module_1/ex00/give_bmi.py
@@ -1,5 +1,12 @@
 def give_bmi(height: list[int | float], weight: list[int | float])\
             -> list[int | float]:
+    """_summary_
+        this fonction take 2 list, height and weight
+        calcul bmi with list donate
+    Returns:
+        list with bmi
+    Error : None if not same len of params
+    """
     if (len(height) != len(weight)):
         print("list len doesn't match")
         return None
@@ -10,4 +17,12 @@ def give_bmi(height: list[int | float], weight: list[int | float])\
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """_summary_
+    send list with true / false if limits was exceed
+    Args:
+        bmi (list[int  |  float]): bmi of users
+        limit (int): value to exceded
+    Returns:
+        list[bool]: user > limit return true else false
+    """
     return [i < limit for i in bmi]
