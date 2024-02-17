b834f81eef4fbbc9077aafc3d26976eea7b78b54
Theo LASSARAT <tlassara@bess-f2r1s19.clusters.42paris.fr>
2024-02-17 15:08:01 +0100

    push ex00

---
+++ module_1/ex00/give_bmi.py
@@ -0,0 +1,13 @@
+def give_bmi(height: list[int | float], weight: list[int | float])\
+            -> list[int | float]:    
+    if (len(height) != len(weight)):
+        print("list len doesn't match")
+        return None
+    final = []
+    for index in range(len(height)):
+        final.append((weight[index] / (height[index] ** 2)))
+    return final
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    return [i < limit for i in bmi]
